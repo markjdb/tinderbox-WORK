814ca608bf2ea64549560335fce1a50781ae17d6
Mark Johnston <markjdb@gmail.com>
2013-03-07 10:21:05 -0500

    Add an update.sh script for our ports tree.

---
+++ contrib/scripts/update_ports.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PORTSTREE=%%
+
+if [ -d $PORTSTREE/ports ]; then
+    cd $PORTSTREE/ports
+    git pull origin svports
+else
+    URL=git@wtllab-bsdbuild-5:/d2/git/base/svports.git
+    git clone --branch svports $URL ports
+fi
