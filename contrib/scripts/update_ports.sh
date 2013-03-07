f7423196414f9422fbf2f856df2a65a0d2837fd5
Mark Johnston <markjdb@gmail.com>
2013-03-07 10:31:10 -0500

    cd to the appropriate dir before cloning.

--- contrib/scripts/update_ports.sh
+++ contrib/scripts/update_ports.sh
@@ -6,6 +6,7 @@ if [ -d $PORTSTREE/ports ]; then
     cd $PORTSTREE/ports
     git pull origin svports
 else
+    cd $PORTSTREE
     URL=git@wtllab-bsdbuild-5:/d2/git/base/svports.git
     git clone --branch svports $URL ports
 fi
